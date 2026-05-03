70e918c27a3882c197d11fc1df814f99cbf7da89
Mritunjay2005 <138350127+Mritunjay2005@users.noreply.github.com>
2026-05-04 00:00:25 +0530

    Add files via upload
    update of 3rd day

---
+++ 8_array/array.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"	
+
+func main() {
+// if we intialize an integer array than it will be initialized with 0 by default
+var mun [4]int
+//print length of array
+ fmt.Println(len(mun))
+
+ mun[0] = 1
+ fmt.Println(mun[0])
+
+
+//by default boolean array will be initialized with false
+ var val[3]bool
+ val[2] = true
+ fmt.Println(val)
+
+
+ //by default string array will be initialized with empty string
+ var name[4]string
+ name[0]="golang"
+ fmt.Println(name)
+
+ num := [5]int{1,2,3,4,5}
+ fmt.Println(num)
+}
