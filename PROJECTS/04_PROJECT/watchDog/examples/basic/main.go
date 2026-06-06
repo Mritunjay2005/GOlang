5f5671f26e25df5f8aa5887812e48f03f3bfcf00
Mritunjay2005 <piyushmritunjay@gmail.com>
2026-06-06 21:50:36 +0530

    add godoc comments to all exported symbols

--- PROJECTS/04_PROJECT/watchDog/examples/basic/main.go
+++ PROJECTS/04_PROJECT/watchDog/examples/basic/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
-    "fmt"
-    "ggithub.com/Mritunjay2005/watchDog"
+	"fmt"
+	"time"
+
+	watchdog "github.com/Mritunjay2005/watchDog"
 )
 
 func main() {
-    w := watchdog.New(watchdog.WithDebounce(100 * time.Millisecond))
-    w.On(watchdog.Modified, "**/*.go", func(e watchdog.Event) {
-        fmt.Printf("changed: %s\n", e.Path)
-    })
-    w.Start(".")
+	w := watchdog.New(watchdog.WithDebounce(100 * time.Millisecond))
+
+	w.On(watchdog.Modified|watchdog.Created, "**/*.go", func(e watchdog.Event) {
+		fmt.Printf("changed: %s\n", e.Path)
+	})
+
+	if err := w.Start("."); err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	fmt.Println("watching for .go file changes... (ctrl+c to stop)")
+	select {}
 }
